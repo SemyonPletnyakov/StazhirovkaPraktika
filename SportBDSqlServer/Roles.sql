261d4a442c49ed408a88073a236923d09112aeab
Семён Семёнович Плетняков <pletnyakov01@mail.ru>
2023-06-08 14:39:04 +0300

    Добавьте файлы проекта.

---
+++ SportBDSqlServer/Roles.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Roles]
+(
+	[Id] INT IDENTITY(1,1), 
+    [Name] VARCHAR(50) NOT NULL, 
+    [ReadSportEvents] BIT NOT NULL,
+	[ChangeSportEvents] BIT NOT NULL,
+	[ChangeUsers] BIT NOT NULL, 
+    CONSTRAINT [PK_Roles] PRIMARY KEY ([Id])
+)
+	
---
+++ SportBDSqlServer/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT IDENTITY(1,1),
+	[RoleId] INT,
+	[Login] VARCHAR(50)  NOT NULL,
+	[Password] VARCHAR(50),
+	[FIO] VARCHAR(50), 
+	CONSTRAINT [PK_Users] PRIMARY KEY ([Id]), 
+	CONSTRAINT [AK_Users_Login] UNIQUE ([Login]),
+    CONSTRAINT [FK_Users_ToRoles] FOREIGN KEY ([RoleId]) REFERENCES [Roles]([Id]) ON DELETE SET NULL
+)
